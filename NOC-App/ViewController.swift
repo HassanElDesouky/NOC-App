f9c6d482ea63f6c3082f1a9ec1acb894b6798e2b
hassaneldesouky <hassaneldesouky@icloud.com>
2019-02-20 18:20:28 +0200

    fix auto layout issue and hide status bar

--- NOC-App/ViewController.swift
+++ NOC-App/ViewController.swift
@@ -14,7 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    // MARK: Remove status bar.
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 
 }
 
