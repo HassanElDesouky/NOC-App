63a10ed16f02e1460dbb49dcc6e49f5218fe68ce
hassaneldesouky <hassaneldesouky@icloud.com>
2019-02-20 17:17:24 +0200

    Initial Commit

---
+++ NOC-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NOC-App
+//
+//  Created by Hassan El Desouky on 2/20/19.
+//  Copyright © 2019 Hassan El Desouky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
