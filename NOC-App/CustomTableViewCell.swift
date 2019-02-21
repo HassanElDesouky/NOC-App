d16e9d2ce88646760036f6f076444040bff5ecf0
hassaneldesouky <hassaneldesouky@icloud.com>
2019-02-21 20:33:57 +0200

    create a custom table view cell

---
+++ NOC-App/CustomTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CustomTableViewCell.swift
+//  NOC-App
+//
+//  Created by Hassan El Desouky on 2/21/19.
+//  Copyright © 2019 Hassan El Desouky. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    //MARK: Outlets
+    @IBOutlet weak var serverImageView: UIImageView!
+    @IBOutlet weak var serverNameLabel: UILabel!
+    @IBOutlet weak var serverIPAddress: UILabel!
+    @IBOutlet weak var serverDeviceIPSubnetMask: UILabel!
+    @IBOutlet weak var statusView: UIView!
+    
+    
+    
+}
---
+++ NOC-App/Server.swift
@@ -0,0 +1,9 @@
+//
+//  Server.swift
+//  NOC-App
+//
+//  Created by Hassan El Desouky on 2/21/19.
+//  Copyright © 2019 Hassan El Desouky. All rights reserved.
+//
+
+import Foundation
